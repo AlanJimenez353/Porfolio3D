e2902c574d1c305da88fef60994ef509386426ba
Alan dev <Alan.jimenez353@gmail.com>
2024-07-12 19:11:02 -0300

    Fix tech stack for mobile resolution

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,18 +4,33 @@ import { technologies } from "../constants"
 import Ball from "./canvas/Ball"
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Check on mount
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Selecciona las tecnologías a mostrar en base al tamaño de la pantalla
+  const displayedTechnologies = isMobile ? technologies.slice(-5) : technologies;
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology)=>(
+      {displayedTechnologies.map((technology) => (
         <div className="w-40 h-40" key={technology.name}>
           <BallCanvas icon={technology.icon} />
-
         </div>
-
-     ))}
-    
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Tech,"tech")
+export default SectionWrapper(Tech, "tech");
